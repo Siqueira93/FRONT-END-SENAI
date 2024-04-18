e8d6ff014474a12397a85350cb1fc61a12f7fd5a
Siqueira93 <raul.soaressiqueira@gmail.com>
2024-04-18 09:54:40 -0300

    .

---
+++ JavaScript/Aula1/materiaDeAula/js/script.js
@@ -0,0 +1,20 @@
+// para se comunicar com o console no navegador faça:
+
+console.log('Olá mundo');
+console.log("Raul");
+
+// caixas de texto (input)
+// window (cria um alerta em caixa no navegador visual)
+// window.prompt("Qual é seu nome?");
+// alert = manda uma mensagem
+// window.alert("front é top");
+// confirm = pergunta podendo sair verdadeiro ou falso
+// window.confirm("deseja baixar esse arquivo novamente?");
+
+var melancia = "uma variavel de teste";
+var ligado = true;
+var numero = 91;
+
+
+
+ 
