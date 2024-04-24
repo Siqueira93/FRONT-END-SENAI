d566c5413c7137b1170403b7f26e129b4d3f77ed
RaulSSiqueira <raul.soaressiqueira@gmail.com>
2024-04-24 14:03:32 -0300

    .

---
+++ JavaScript/Aula3/script.js
@@ -0,0 +1,13 @@
+
+function teladeLogin() {
+   let login = "raul123";
+   let senha = Number;
+
+   window.prompt("Qual o seu Login");
+    console.log(login)
+   if (login == "raul123" ) {
+      window.alert("Bem vindo senhor");
+   } else {
+         window.alert("Forasteiro!!");
+       }
+   }
