294b75476e85931b44f5251ab1e649c03d0fdad0
Siqueira93 <raul.soaressiqueira@gmail.com>
2024-05-02 09:59:06 -0300

    .

--- JavaScript/Aula5/atividade/script.js
+++ JavaScript/Aula5/atividade/script.js
@@ -1,49 +1,67 @@
 
+// Exercicio 1
+function exercicio1() {
+    let resposta1 = document.querySelector("#respostaEx1");
 
-class carro {
-    constructor (marca, modelo, ano, cor, velocidadeAtual){
-        this.marca = marca;
-        this.modelo = modelo;
-        this.ano = ano;
-        this.cor = cor;
-        this.velocidadeAtual = velocidadeAtual;
+    class Carro {
+        // constructor para auxiliar na organização dos atributos
+        constructor(marca,modelo,ano,cor,velMax) {
+            // IMPORTANTE: se for mexer com objeto é THIS
+            this.marca = marca;
+            this.modelo = modelo;
+            this.ano = ano;
+            this.cor = cor;
+            this.velMax = velMax;
+            // Aqui defino o padrão que o usuário não pode alterar
+            // Não é encaixado no constructor porque é objeto so que modavel pelo dev
+            this.velAtual = 0;
+            // criamos um novo atributo devido a pergunta do exercicio
+            this.acelerar = function(){
+
+            }
+
+        }
+
+    }
     
-    }   
+    // criando um teste 
+    var carro1 = new Carro("Fiat","Uno",1990,"vermelho ferrari",140);
+    
+
+    // imprimir na tela a resposta1
+    resposta1.innerHTML = `
+    <div id="carro1resposta">
+
+        <p>Marca: ${carro1.marca} </p>
+        <p>Modelo: ${carro1.modelo} </p>
+        <p>Ano: ${carro1.ano} </p>
+        <p>Cor: ${carro1.cor} </p>
+        <p>Velocidade Máxima: ${carro1.velMax} </p>
+        <p>Velocidade Atual: ${carro1.velAtual} </p>
+        <input type="number" id="acelerar">
+
+    </div>
+    `;
+
 }
 
-var Uno = new carro();
-    Uno.marca = "Fiat";
-    Uno.modelo = "Uno";
-    Uno.ano = "2024";
-    Uno.cor = "Dourado";
-    Uno.velocidadeAtual = 0;
 
-function calcularVelocidade(resultado){
-    let resultado
-    if (velocidadeAtual = 1000) {
-        console.log("parado")
-    } else {}
+
+// Exercicio 2
+
+
+
+
+// Exercicio 3
+function fecharPopUp() {
+    let resposta3 = document.querySelector("#respostaEx3");
+
+    resposta3.style.display = "none";
+
 }
 
+function exercicio3(){
+    let resposta3 = document.querySelector("#respostaEx3");
 
-
-
-
-
-// velocidadeMaxima(valor) {
-    //     this.velocidadeAtual += valor;
-
-    //     let resultado = document.querySelector("#infocar");
-        
-    //     resultado.innerHTML = 
-    //     `<p>VELOCIDADE É DE: ${this.velocidadeAtual}</p>`;
-    // }
-
-// var Uno = new carro();
-// Uno.marca = "Fiat";
-// Uno.modelo = "Uno";
-// Uno.ano = "2024";
-// Uno.cor = "Dourado";
-// Uno.velocidadeAtual = 0;
-
-// console.log(Uno)
+    resposta3.style.display = "flex";
+}
