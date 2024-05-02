3d711a7c2736205b0f499bc3f35fa0cf6019e3f0
Siqueira93 <raul.soaressiqueira@gmail.com>
2024-05-02 11:44:37 -0300

    .

--- JavaScript/Aula5/atividade/script.js
+++ JavaScript/Aula5/atividade/script.js
@@ -1,5 +1,6 @@
 
 // Exercicio 1
+var carro1
 function exercicio1() {
     let resposta1 = document.querySelector("#respostaEx1");
 
@@ -16,17 +17,36 @@ function exercicio1() {
             // Não é encaixado no constructor porque é objeto so que modavel pelo dev
             this.velAtual = 0;
             // criamos um novo atributo devido a pergunta do exercicio
-            this.acelerar = function(){
-
+            this.acelerar = function(VelocidadeAcelerada){
+                if(this.velAtual + VelocidadeAcelerada > this.velMax) {
+                    alert("Não permitido")
+                }else {
+                    this.velAtual += VelocidadeAcelerada 
+                   
+                    resposta1.innerHTML = `
+                <div id="carro1resposta">
+                    <p>Marca: ${carro1.marca} </p>
+                    <p>Modelo: ${carro1.modelo} </p>
+                    <p>Ano: ${carro1.ano} </p>
+                    <p>Cor: ${carro1.cor} </p>
+                    <p>Velocidade Máxima: ${carro1.velMax} </p>
+                    <p>Velocidade Atual: ${carro1.velAtual} </p>
+                    <input type="number" id="acelerar">
+                    <button type="button" onclick="acelerar()"> Acelerar </button>
+                </div>
+                `;
+                
+                }
+                
             }
+            
 
         }
 
     }
     
     // criando um teste 
-    var carro1 = new Carro("Fiat","Uno",1990,"vermelho ferrari",140);
-    
+    carro1 = new Carro("Fiat","Uno",1990,"vermelho ferrari",140);
 
     // imprimir na tela a resposta1
     resposta1.innerHTML = `
@@ -39,15 +59,46 @@ function exercicio1() {
         <p>Velocidade Máxima: ${carro1.velMax} </p>
         <p>Velocidade Atual: ${carro1.velAtual} </p>
         <input type="number" id="acelerar">
-
+        <button type="button" onclick="acelerar()"> Acelerar </button>
     </div>
     `;
+   
+}
+
+function acelerar() {
+    let inputAcelerar = Number(document.querySelector("#acelerar").value);
+    
+    carro1.acelerar(inputAcelerar);
+
 
 }
 
 
-
 // Exercicio 2
+var usuarios = [];
+function exercicio2(){
+    let resposta2 = document.querySelector("#respostaEx2");
+
+    resposta2.innerHTML = `
+        <input type="text" id="nome">
+        <button type="button" onclick="cadastrar()"> Cadastrar </button>
+    `
+}
+
+function cadastrar() {
+    // VALUE (para pegar somente o que o usuario digitar)
+    let input = document.querySelector("#nome").value;
+    let resposta2 = document.querySelector("#respostaEx2");
+
+    // adicionando a lista "usuarios" 
+    // leia o que digitaram na caixa "input"
+    usuarios.push(input);
+    // depois que o usuario digitar a "resposta" no input
+    // imprima no html
+    resposta2.innerHTML += input;
+
+
+}
 
 
 
