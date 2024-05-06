6d79c8877f745c468410ea7f98bc60b5143d3401
Siqueira93 <raul.soaressiqueira@gmail.com>
2024-05-06 11:15:06 -0300

    .

--- JavaScript/Aula6/script.js
+++ JavaScript/Aula6/script.js
@@ -22,11 +22,44 @@ function pesquisar(){
     var pesquisa = filmes.includes("Pokemon");
 
     var resposta2 = document.querySelector("#resposta2");
-    resposta2.innerHTML = pesquisa;
+   
+    if (pesquisa == true) {
+        resposta2.innerHTML = `Opa o termo foi encontrado!`;
+    } else {
+        resposta2.innerHTML = `O termo não foi encontrado!`;
+    }
+    
+    // indexOf verufica se existe, também retorna indice
+    var termoPesquisado = "teste";
+    var pesquisa2 = filmes.indexOf(termoPesquisado);
+
+    if(pesquisa2 == -1){
+        resposta2.innerHTML += `<br> O elemento digitado não foi encontrado`;
+    } else {
+        resposta2.innerHTML += `<br> O elemento ${filmes[pesquisa]} foi encontrado!`;
+    }
 
 }
+
+
+// o pad() serve para buscar e substuir alguma informação
+function pad(){
+    var nome = "Raul";
+    var sobreNome = "Soares";
+    var cartaoFinal = "9999";
+    var totalCaracteres = (nome + sobreNome).length + 1;
+    var resposta3 = document.querySelector("#resposta3");
+    
+
+    resposta3.innerHTML = nome.padStart(totalCaracteres , sobreNome);
+    resposta3.innerHTML += "<br>" + nome.padEnd(8 , sobreNome);
+    resposta3.innerHTML += "<br>" + cartaoFinal.padStart(16, "*");
+}
+
+
 
 
 //executar functions
 replaceEcma();
 pesquisar();
+pad();
