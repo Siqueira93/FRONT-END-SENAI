18b14f35b164237bb4826507e2d886dba57f75ee
RaulSSiqueira <raul.soaressiqueira@gmail.com>
2024-05-15 13:40:00 -0300

    .

---
+++ JavaScript/Aula7 - jquery/testandoSiteJquery/script.js
@@ -0,0 +1,29 @@
+
+
+// menuu
+// logo
+var logo = $("#logoP").html(`<img src='images/png-logo.png' alt='logo'>`);
+
+// botao home
+var home = $("#home").html("<a>HOME</a>");
+// botao pag
+var pag = $("#pag2").html("<a>PAGINA2</a>");
+
+
+
+
+// main
+var informativo1 = $("#info").html("Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque enim amet laborum id nam dolorum, repudiandae deleniti accusamus est similique alias expedita placeat. Ea quod dolore m        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque enim amet laborum id nam dolorum, repudiandae deleniti accusamus est similique alias expedita placeat. Ea quod dolore molestiae totam excepturi architecto")
+
+var img1 = $("#img").html(`<img src='images/lua.jpg' alt='logo' height='700px' width='98%'>`);
+
+var informativo2 = $("#info2").html("Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque enim amet laborum id nam dolorum, repudiandae deleniti accusamus est similique alias expedita placeat. Ea quod dolore m        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque enim amet laborum id nam dolorum, repudiandae deleniti accusamus est similique alias expedita placeat. Ea quod dolore molestiae totam excepturi architecto");
+
+var img2 = $("#img2").html(`<img src='images/luaVermelha.jpg' alt='logo' height='700px' width='98%'>`);
+
+var informativo3 = $("#info3").html("Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque enim amet laborum id nam dolorum, repudiandae deleniti accusamus est similique alias expedita placeat. Ea quod dolore m        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque enim amet laborum id nam dolorum, repudiandae deleniti accusamus est similique alias expedita placeat. Ea quod dolore molestiae totam excepturi architecto");
+
+var img3 = $("#img3").html(`<img src='images/luaVarias.jpg' alt='logo' height='700px' width='98%'>`);
+
+
+
